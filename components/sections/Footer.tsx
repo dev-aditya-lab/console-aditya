1f6a0f8fe5f0074a11e0282705a9f2dcaa6ca982
adii <ad1123itya@gmail.com>
2025-07-07 11:12:17 +0530

    feat: update Footer component with new layout and text; add Separator component

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='w-full py-10 text-center flex items-center justify-center bg-gray-800 text-white'>
-        <h1>
-            Made with keyboard rage & caffeine ☕ | © {new Date().getFullYear()} consoleAditya.dev
-        </h1>
-    </div>
+     <footer className="w-full ">
+      <div className="max-w-6xl mx-auto px-6 py-10 flex flex-col items-center gap-6">
+        {/* Footer Text */}
+        <p className="text-sm text-center leading-relaxed">
+          Made with keyboard rage & caffeine ☕, 💻, &{" "}
+          <span className="font-semibold text-foreground">Next.js + TypeScript</span>  
+          <br />
+          © {new Date().getFullYear()} <span className="font-medium text-foreground">Aditya Kumar Gupta</span>. All rights reserved.
+        </p>
+      </div>
+    </footer>
   )
 }
